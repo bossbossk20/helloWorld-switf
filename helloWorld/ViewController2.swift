8f711cd94972d3297aa8a2757a7f267332899ba7
thanawat <tha_chookes@hotmail.com>
2016-06-23 23:11:31 +0700

    init

--- helloWorld/ViewController2.swift
+++ helloWorld/ViewController2.swift
@@ -1,36 +1,31 @@
 //
-//  ViewController.swift
+//  ViewController2.swift
 //  helloWorld
 //
-//  Created by thanawat on 6/22/2559 BE.
+//  Created by thanawat on 6/23/2559 BE.
 //  Copyright © 2559 thanawat. All rights reserved.
 //
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController2: UIViewController {
     
-    @IBOutlet weak var textfield2: UITextField!
-    @IBOutlet var myLabel : UILabel?
-    @IBOutlet var myTextField : UITextField?
-    @IBAction func Textfield2(sender: AnyObject) {
-        myLabel!.text = textfield2.text;
-    }
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
-    @IBAction func didPressView() {
-        myLabel!.text = myTextField!.text
+    @IBAction func clickBackButton(sender: AnyObject) {
+        self.dismissViewControllerAnimated(true, completion: nil)
     }
-
-
+    
 }
+
 
